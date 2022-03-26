b1e731cdd68e7d9011d20b5619f740bf002455f3
Newliberties <newliberties@yahoo.com>
2022-03-26 16:06:48 -0700

    let us fix footer

--- routes/books.js
+++ routes/books.js
@@ -996,9 +996,10 @@ router.use(function (err, req, res, next) {
 });
 // Title Case Genre
 function titleCase(str) {
-	return str.toLowerCase().split(' ').map(function (word) {
+	return str.trim().toLowerCase().split(' ').map(function (word) {
 		return (word.charAt(0).toUpperCase() + word.slice(1));
 	}).join(' ');
+
 }
 
 module.exports = router
